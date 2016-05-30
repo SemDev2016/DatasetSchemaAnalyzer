48ea9214811db3817cd7ef04ea56c04fd0b8cb38
Martin Ledvinka <ledvima1@fel.cvut.cz>
2016-05-30 17:09:11 +0300

    Ideas described. Engine partial implementation

--- js/engine/Engine.js
+++ js/engine/Engine.js
@@ -3,12 +3,29 @@ import Query from './Query';
 
 var Engine = {
 
-    getAllClasses() {
-        request.get('http://dbpedia.org/sparql?query=' + encodeURIComponent(Query.ALL_CLASSES)).accept('json').end(function (err, resp) {
+    classes: null,
+    endpoint: 'http://dbpedia.org/sparql',
+
+    getAllClasses: function() {
+        request.get(this.endpoint + '?query=' + encodeURIComponent(Query.ALL_CLASSES)).accept('json').end(function (err, resp) {
             if (!err) {
-                console.log(resp.body);
+                this.classes = resp.body.results.bindings.map(function(item) {
+                    return item['x']['value'];
+                });
+                console.log(this.classes);
+                this.getSuperclasses();
             }
-        });
+        }.bind(this));
+    },
+
+    getSuperclasses: function() {
+        var results = true;
+        for (var i = 0, len = this.classes.length; i < len; i++) {
+            var q = Query.SUPERCLASSES.replace('?y', '<' + this.classes[i] + '>');
+            request.get(this.endpoint + '?query' + encodeURIComponent(q)).accept('json').end(function(err, resp) {
+
+            }.bind(this));
+        }
     }
 };
 
--- js/engine/Query.js
+++ js/engine/Query.js
@@ -1,5 +1,14 @@
 module.exports = {
     ALL_CLASSES: 'SELECT DISTINCT ?x WHERE {'
                     + '?y rdf:type ?x'
-                + '}'
+                + '}',
+
+    SUPERCLASSES: 'SELECT DISTINCT ?x WHERE {'
+                    + '?y rdfs:subClassOf ?x'
+                + '}',
+
+    PROPERTY_COUNT: 'Select Distinct ?property (Count(?x) as ?CountX)'
+                    + 'Where'
+                        + '{?LowestLevelClass ?property ?x .}'
+                    + 'Group by ?property'
 }
