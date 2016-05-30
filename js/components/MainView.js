4e6913f0316bbf13bff7ee7f3011843ab13564c7
Martin Ledvinka <ledvima1@fel.cvut.cz>
2016-05-30 15:58:46 +0300

    First query test.

--- js/components/MainView.js
+++ js/components/MainView.js
@@ -2,10 +2,26 @@
 
 import React from 'react';
 import {Button, Navbar, NavBrand} from 'react-bootstrap';
+import request from 'superagent';
 
 export default class MainView extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        this.getAllClasses();
+    }
+
+    getAllClasses() {
+        var query = 'SELECT DISTINCT ?x WHERE {'
+        + '?y rdf:type ?x'
+        + '}';
+        request.get('http://dbpedia.org/sparql?query=' + encodeURIComponent(query)).accept('json').end(function (err, resp) {
+            if (!err) {
+                console.log(resp.body);
+            }
+        });
     }
 
     render() {
